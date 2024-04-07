2a9f9bbf5d8e1b722c25926954af574249a34b4a
edujeeho <121836637+edujeeho@users.noreply.github.com>
2024-04-08 02:53:58 +0900

    Update TodoController.java

--- src/main/java/com/example/todo/TodoController.java
+++ src/main/java/com/example/todo/TodoController.java
@@ -4,6 +4,7 @@ import com.example.todo.entity.Todo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
